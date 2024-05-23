f039f13f98c4dc88c3fdcb8e68ec72f15cafeada
lidingxin <lidingxin258@qq.com>
2024-05-23 20:50:02 +0800

    命令处理函数

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,4 +1,5 @@
 var terminalElement = document.querySelector('.terminal');
+var terminalShow = document.querySelector('.terminal-text-show');
 var textElement = terminalElement.querySelector('.terminal-text');
 var terminalCursor = document.querySelector('.terminal-cursor');
 
@@ -10,7 +11,13 @@ function initTerminal() {
     textElement.textContent = textContent;
     // 设置监听键盘输入
     terminalElement.addEventListener('keydown', function (event) {
-        if (event.key === 'Backspace') {
+        if (event.key === 'Enter') {
+            // 当按下回车键时，执行handleCommand函数
+            handleCommand(textContent.slice(minLength));
+            // 重新初始化终端
+            initTerminal();
+        }
+        else if (event.key === 'Backspace') {
             // 只有当textContent的长度大于最小长度时，才允许删除字符
             if (textContent.length > minLength) {
                 textContent = textContent.slice(0, -1);
@@ -27,5 +34,46 @@ function initTerminal() {
         event.preventDefault();
     });
 }
+/*
+// 设置网页转换的动画
+barba.init({
+    transitions: [{
+      name: 'opacity-transition',
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0
+        });
+      }
+    }]
+  });
+*/
+
+function handleCommand(command) {
+    var commands = command.split(' ');
+    var smileFace = ':) ';
+    var unhappyFace = ':( ';
+    switch (commands[0]) {
+        case 'help':
+            response = smileFace + "查看文章列表:show all  阅读某篇文章:read yy-mm-dd "
+            break;
+        case 'show':
+            window.location.href = '/articles';
+            break;
+        case 'read':
+            if (commands.length > 1) {
+                window.location.href = '/articles/' + article;
+            } else {
+                response = unhappyFace + '请指定日期(yy-mm-dd)或文章名';
+            }
+            break;
+    }
+    terminalShow.textContent = response;
+}
+
 
 initTerminal();
